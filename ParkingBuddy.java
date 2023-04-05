f5db76f15b6c923dc649a65afb15902e4222756e
Osrosario <114501083+Osrosario@users.noreply.github.com>
2023-04-05 17:25:34 -0400

    Added Function to Generate Spaces

--- ParkingBuddy.java
+++ ParkingBuddy.java
@@ -1,15 +1,19 @@
-import java.lang.reflect.AnnotatedArrayType;
+import java.util.ArrayList;
 import java.util.Scanner;
-
-import javax.lang.model.util.ElementScanner14;
-import javax.security.auth.callback.ChoiceCallback;
+import java.util.Random;
 
 public class ParkingBuddy 
 {
     private static Scanner userInput = new Scanner(System.in);
     private static int answer = 0;
     private static boolean isDone = false;
-    
+    private static ArrayList<ParkingSpace> genericLotList = new ArrayList<ParkingSpace>();
+    private static Random random = new Random();
+
+    private static String[] priorityList = { "H", "1", "2", "3" };
+    private static int[] capacityList = { 20, 20, 30, 40 };
+    private static boolean[] statusList = { true, false };
+
     private static String validateInt()
     {
         String input;
@@ -52,25 +56,49 @@ public class ParkingBuddy
         return choice;
     }
 
+    private static void generateSpaces()
+    {
+        genericLotList.clear();
+        
+        for (int i = 0; i < priorityList.length; i++)
+        {
+            for (int j = 0; j < capacityList[i]; j++)
+            {
+                int randNum = random.nextInt(0, 2);
+                ParkingSpace newSpace = new ParkingSpace(priorityList[i], statusList[randNum]);
+                genericLotList.add(newSpace);
+            }
+        }
+    }
+
+    private static void refreshSpaces()
+    {
+        for (ParkingSpace space : genericLotList)
+        {
+            int randNum = random.nextInt(0, 2);
+            space.setSpaceFull(statusList[randNum]);
+        }
+    }
+
     private static void printParkingSpaces()
     {
         System.out.println("\nPriority | Space Number | Availiability");
 
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < genericLotList.size(); i++)
         {
             System.out.println("------------------------------------------");
             
-            if (Integer.toString(i).length() == 1)
+            if (Integer.toString(i + 1).length() == 1)
             {
-                System.out.println(1 + "        | " + i + "            | Open");
+                System.out.println(genericLotList.get(i).getPriority() + "        | " + (genericLotList.indexOf(genericLotList.get(i)) + 1) + "            | " + genericLotList.get(i).isSpaceFull());
             }
-            else if (Integer.toString(i).length() == 2)
+            else if (Integer.toString(i + 1).length() == 2)
             {
-                System.out.println(1 + "        | " + i + "           | Open");
+                System.out.println(genericLotList.get(i).getPriority() + "        | " + (genericLotList.indexOf(genericLotList.get(i)) + 1) + "           | " + genericLotList.get(i).isSpaceFull());
             }
-            else if (Integer.toString(i).length() == 3)
+            else if (Integer.toString(i + 1).length() == 3)
             {
-                System.out.println(1 + "        | " + i + "          | Open");
+                System.out.println(genericLotList.get(i).getPriority() + "        | " + (genericLotList.indexOf(genericLotList.get(i)) + 1) + "          | " + genericLotList.get(i).isSpaceFull());
             }
         }
     }
@@ -93,11 +121,16 @@ public class ParkingBuddy
         }
         else
         {
-            viewParkingSpaces(answer);
+            if (answer == 4)
+            {
+                answer = 3;
+            }
+
+            viewParkingLot(answer);
         }    
     }
 
-    private static void viewParkingSpaces(int choice)
+    private static void viewParkingLot(int choice)
     {
         System.out.println("\nWhich lot would you like view?");
 
@@ -136,33 +169,46 @@ public class ParkingBuddy
         }
         else
         {
-            printParkingSpaces();
+            refreshSpaces();
+            viewParkingSpaces(choice);
+        }
+    }
 
-            System.out.println("\nDo you want to view another lot?");
-            System.out.println("1. Yes");
-            System.out.println("2. Return to main menu");
-            System.out.println("0. Exit Application");
+    private static void viewParkingSpaces(int choice)
+    {
+        printParkingSpaces();
 
-            System.out.print("\nPlease choose a number: ");
-            answer = validateAnswer(answer);
+        System.out.println("\nDo you want to view another lot?");
+        System.out.println("1. Yes");
+        System.out.println("2. Refresh Availability");
+        System.out.println("3. Return to Main Menu");
+        System.out.println("0. Exit Application");
 
-            if (answer == 0)
-            {
-                isDone = true;
-            }
-            else if (answer == 1)
-            {
-                viewParkingSpaces(choice);
-            }
-            else if (answer == 2)
-            {
-                mainMenu();
-            }
+        System.out.print("\nPlease choose a number: ");
+        answer = validateAnswer(answer);
+
+        if (answer == 0)
+        {
+            isDone = true;
+        }
+        else if (answer == 1)
+        {
+            viewParkingLot(choice);
+        }
+        else if (answer == 2)
+        {
+            refreshSpaces();
+            viewParkingSpaces(choice);
+        }
+        else if (answer == 3 || answer == 4)
+        {
+            mainMenu();
         }
     }
     
     public static void main(String[] args) 
     {
+        generateSpaces();
         mainMenu();
     }
 }
